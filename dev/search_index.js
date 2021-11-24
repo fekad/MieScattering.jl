b6fc1837ac15e53de7d8c83cc434900ab5f95aee
Documenter.jl <documenter@juliadocs.github.io>
2021-11-24 23:37:21 +0000

    build based on 60492a6

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#MieScattering.jl","page":"Home","title":"MieScattering.jl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MieScattering.jl","category":"page"}]
+}
